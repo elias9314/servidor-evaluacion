f95c3638d2a7b6334d5c9cae82c302b70d9ef055
GERFSHARP <angiecsb982@gmail.com>
2020-03-15 16:03:48 -0500

    storage

--- database/migrations/2014_01_04_2_create_carreras_table.php
+++ database/migrations/2014_01_04_2_create_carreras_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;  
     
-class CreateCarrerasTable extends Migration
+class CreateCarrerasTable extends Migration 
 {
     /**
      * Run the migrations.
