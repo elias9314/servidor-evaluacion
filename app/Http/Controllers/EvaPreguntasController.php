b25670a5d6c88463502cec20a30e5a45dc006cb2
elias9314 <48599005+elias9314@users.noreply.github.com>
2020-03-18 18:21:53 -0500

    este si vale el crear preguntas

--- app/Http/Controllers/EvaPreguntasController.php
+++ app/Http/Controllers/EvaPreguntasController.php
@@ -18,26 +18,25 @@ class EvaPreguntasController extends Controller
     }
 
     public function createPregunta(Request $request){
-        $data=$request->json()->all();
 
-     $dataPregunta=$data;
-     $pregunta=EvaPregunta::create([
-         'tipo_evaluacion_id'=>$dataPregunta['tipo_evaluacion_id'],
-         'codigo'=>$dataPregunta['codigo'],
-         'orden'=>$dataPregunta['orden'],
-         'nombre'=>$dataPregunta['nombre'],
-         'tipo'=>$dataPregunta['tipo'],
-         'estado'=>$dataPregunta['estado'],
-     ]);
-     return response()->json([$pregunta],200);
-        //$data=$request->json()->all();
-     //$dataPregunta =$data['eva_preguntas'];
-     //$dataTipoEvaluacion= $data['tipo_evaluaciones'];
-     //$evaluacion=TipoEvaluacion::find($dataTipoEvaluacion['id']);
-     //$response=$evaluacion->pregunta()->create($dataPregunta);
+    // $dataPregunta=$data;
+     //$pregunta=EvaPregunta::create([
+      //   'tipo_evaluacion_id'=>$dataPregunta['tipo_evaluacion_id'],
+      //   'codigo'=>$dataPregunta['codigo'],
+      //   'orden'=>$dataPregunta['orden'],
+      //   'nombre'=>$dataPregunta['nombre'],
+      //   'tipo'=>$dataPregunta['tipo'],
+      //   'estado'=>$dataPregunta['estado'],
+     //]);
+    // return response()->json([$pregunta],200);
+        $data=$request->json()->all();
+     $dataPregunta =$data['eva_preguntas'];
+     $dataTipoEvaluacion= $data['tipo_evaluaciones'];
+     $evaluacion=TipoEvaluacion::find($dataTipoEvaluacion['id']);
+     $response=$evaluacion->pregunta()->create($dataPregunta);
      //EvaPregunta::create($data);
        // return response()->json(['message'=>'pregunta creada correctamente'],200);
-    //return response()->json([$response],200);
+    return response()->json([$response],200);
     }
 
 
