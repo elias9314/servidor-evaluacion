c7b01ab97523ff584fd61a90256da8c2a7c3d9fd
GERFSHARP <angiecsb982@gmail.com>
2020-04-18 16:47:57 -0500

    cambios create

--- app/Http/Controllers/ResultadosController.php
+++ app/Http/Controllers/ResultadosController.php
@@ -16,26 +16,6 @@ class ResultadosController extends Controller
         return response()->json($resultados,200);
     }
 
-    // public function createresultado(Request $request){
-    //     $data = $request->json()->all();
-    //     $dataResultado = $data['resultados'];
-    //     $resultados = new Resultado([
-    //         'valor' => $dataResultado['valor'],
-    //         'tipo' => $dataResultado['tipo'],
-    //         'estado' => $dataResultado['estado']
-    //     ]);
-    //     $respuesta = EvaPreguntaEvaRespuesta::findOrFail($data['resultados']['eva_pregunta_eva_respuesta']['id']);
-    //     $estudiante = Estudiante::findOrFail($dataResultado['estudiante']['id']);
-    //     $docenteasignatura = DocenteAsignatura::findOrFail($dataResultado['docente_asignatura']['id']);
-
-    //     $resultados->eva_pregunta_eva_respuesta()->associate($respuesta);
-    //     $resultados->estudiante()->associate($estudiante);
-    //     $resultados->docente_asignatura()->associate($docenteasignatura);
-    //     $resultados->save();
-    //     return response()->json(['resultados' => $resultados], 201);
-        
-    // }
-
     public function getIdDocenteAsignatura(Request $request)
     {
         $asignaturaDocente = DocenteAsignatura::where('docente_id',$request->id)->first();
